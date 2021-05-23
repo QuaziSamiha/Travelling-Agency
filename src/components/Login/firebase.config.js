9fccafe1387293149d182bea0e7fe2ba3581cc8b
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-05-24 00:12:02 +0600

    basic firebase settings done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5H_0cvEdh4CAKT5ZMaJt8z_v3vDJNnbo",
+    authDomain: "assignment-9-201ad.firebaseapp.com",
+    projectId: "assignment-9-201ad",
+    storageBucket: "assignment-9-201ad.appspot.com",
+    messagingSenderId: "616383497948",
+    appId: "1:616383497948:web:ea3f719738e956de0cd619"
+};
+
+export default firebaseConfig;
