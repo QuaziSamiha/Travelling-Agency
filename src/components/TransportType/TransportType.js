9ba935e977a4233485eac3d15754a7c5831463ed
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-05-29 22:47:02 +0600

    destination , transport and map trying

--- src/components/TransportType/TransportType.js
+++ src/components/TransportType/TransportType.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router';
+import Map from '../../images/Map.png';
+import './TransportType.css';
 
 const TransportType = () => {
     const { transportType } = useParams();
@@ -28,8 +30,8 @@ const TransportType = () => {
 
 
     const handleSearch = (event) => {
-        if(event.type === 'submit'){
-            const passengerInfo = {...passenger};
+        if (event.type === 'submit') {
+            const passengerInfo = { ...passenger };
             passengerInfo.success = true;
             setPassenger(passengerInfo);
         }
@@ -39,24 +41,31 @@ const TransportType = () => {
 
 
     return (
-        <div style={{ paddingTop: '250px', margin: '0 20%' }}>
+        <div className='destination-map' style={{ paddingTop: '250px', margin: '0 20%' }}>
             <h1 className='text-info'>Welcome to travel by {transportType}</h1>
-            <form onSubmit={handleSearch} className='border rounded-3 p-3 m-'>
-                <fieldset>
-                    <label className='text-info' htmlFor="Pick from">Pick from</label> <br />
-                    <input onBlur={handleBlur} className='form-control' type="text" name="pick-from" required /> <br />
-                    <label className='text-info' htmlFor="Pick to">Pick to</label> <br />
-                    <input onBlur={handleBlur} className='form-control' type="text" name="pick-to" required /> <br />
-                    <input className='bg-info form-control' type="submit" value="Search" />
-                </fieldset>
-            </form>
+            <div className='transport-destination'>
+                {
+                    passenger.success === false ?
+                        <form onSubmit={handleSearch} className='border rounded-3 p-3 m-'>
+                            <fieldset>
+                                <label className='text-info' htmlFor="Pick from">Pick from</label> <br />
+                                <input onBlur={handleBlur} className='form-control' type="text" name="pick-from" required /> <br />
+                                <label className='text-info' htmlFor="Pick to">Pick to</label> <br />
+                                <input onBlur={handleBlur} className='form-control' type="text" name="pick-to" required /> <br />
+                                <input className='bg-info form-control' type="submit" value="Search" />
+                            </fieldset>
 
-            {
-            passenger.success && <div>
-                <p>{passenger.source}</p>
-                <p>{passenger.destination}</p>
+                        </form>
+                        :
+                        <div className='bg-info text-dark p-4 m-5 border rounded-2'>
+                            <h6>{passenger.source}</h6>
+                            <h6>{passenger.destination}</h6>
+                        </div>
+                }
             </div>
-            }
+            <div className='map-div'>
+                <img src={Map} alt="" />
+            </div>
         </div>
     );
 };
