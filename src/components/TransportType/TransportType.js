47159bfb73793840e65dba612ca6a2753dccd3fd
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-05-30 00:12:23 +0600

    main task almost done but this site is not respnsive yet

--- src/components/TransportType/TransportType.js
+++ src/components/TransportType/TransportType.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import Map from '../../images/Map.png';
+import Bike from '../../images/bike.png';
+import Car from '../../images/car.png';
+import Bus from '../../images/bus.png';
+import Train from '../../images/train.png'
+import People from '../../images/peopleicon.png';
 import './TransportType.css';
 
 const TransportType = () => {
@@ -39,11 +44,24 @@ const TransportType = () => {
         event.preventDefault();
     }
 
+    let transport;
+    if (transportType === 'bike') {
+        transport = Bike;
+    }
+    if (transportType === 'car') {
+        transport = Car;
+    }
+    if (transportType === 'bus') {
+        transport = Bus;
+    }
+    if (transportType === 'train') {
+        transport = Train;
+    }
 
     return (
         <div className='destination-map'>
             <div className='transport-destination'>
-            <h1 className='text-info'>Welcome to travel by {transportType}</h1>
+                <h1 className='text-info'>Welcome to travel by {transportType}</h1>
                 {
                     passenger.success === false ?
                         <form onSubmit={handleSearch} className='border rounded-3 p-3 m-'>
@@ -57,9 +75,16 @@ const TransportType = () => {
 
                         </form>
                         :
-                        <div className='bg-info text-dark p-4 m-5 border rounded-2'>
-                            <h6>{passenger.source}</h6>
-                            <h6>{passenger.destination}</h6>
+                        <div className='bg-info text-dark p-5 m-2 border rounded-2'>
+                            <div className='bg-primary p-3 m-3 border rounded-2'>
+                                <h6>{passenger.source}</h6>
+                                <h6>{passenger.destination}</h6>
+                            </div>
+                            <div className='icons bg-light p-3 m-3 border rounded-2'>
+                                <img className='transport-img' src={transport} alt="" /> <p><b>{transportType}</b></p>
+                                <img className='people-img' src={People} alt="" /> <p><b>4</b></p>
+                                <p className='px-5'><b>$67</b></p>
+                            </div>
                         </div>
                 }
             </div>
