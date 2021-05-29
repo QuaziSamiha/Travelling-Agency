b944d565ba8db81c0ef03f8dcc838b2e3e26666b
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-05-29 09:39:19 +0600

    login component refactor done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,43 +1,34 @@
 import React, { useContext, useState } from 'react';
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from './firebase.config';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router';
 import GoogleLogo from '../../images/google-logo.png';
 import './Login.css';
+import { createUserWithEmailAndPassword, handleGoogleSignIn, initializeLoginFramework, signInWithEmailAndPassword } from './LoginManager';
 
-if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-}
-
+initializeLoginFramework();
 
 const Login = () => {
-
+    // context API
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
 
     // codes for private route
     const history = useHistory();
     const location = useLocation();
     const { from } = location.state || { from: { pathname: '/' } }
 
+    const handleResponse = (res, redirect) => {
+        setUser(res);
+        setLoggedInUser(res);
+        if (redirect) {
+            history.replace(from);
+        }
+    }
+
     // sign in using google
-    const handleGoogleSignIn = () => {
-        firebase.auth()
-            .signInWithPopup(googleProvider)
+    const googleSignIn = () => {
+        handleGoogleSignIn()
             .then(res => {
-                const { displayName, email } = res.user;
-                const signedInUser = {
-                    name: displayName,
-                    email: email
-                };
-                setLoggedInUser(signedInUser);
-                history.replace(from);
-            })
-            .catch(error => {
-                console.log(error);
+                handleResponse(res, true);
             })
     }
 
@@ -72,43 +63,17 @@ const Login = () => {
 
     const handleSubmit = (event) => {
         if (newUser && user.email && user.password) {
-            firebase.auth()
-                .createUserWithEmailAndPassword(user.email, user.password)
+            createUserWithEmailAndPassword(user.name, user.email, user.password)
                 .then(res => {
-                    const newUserInfo = { ...user };
-                    newUserInfo.error = '';
-                    newUserInfo.success = true;
-                    setUser(newUserInfo);
-                    setLoggedInUser(newUserInfo);
-                    history.replace(from);
+                    handleResponse(res, true);
                 })
-                .catch((error) => {
-                    const newUserInfo = { ...user };
-                    newUserInfo.success = false;
-                    newUserInfo.error = error.message + '!!!';
-                    setUser(newUserInfo);
-                });
         }
-
         if (!newUser && user.email && user.password) {
-            firebase.auth()
-                .signInWithEmailAndPassword(user.email, user.password)
+            signInWithEmailAndPassword(user.email, user.password)
                 .then(res => {
-                    const userInfo = { ...user };
-                    userInfo.error = '';
-                    userInfo.success = true;
-                    setUser(userInfo);
-                    setLoggedInUser(userInfo);
-                    history.replace(from);
-                })
-                .catch(error => {
-                    const userInfo = { ...user };
-                    userInfo.error = error.message;
-                    userInfo.success = false;
-                    setUser(userInfo)
+                    handleResponse(res, true);
                 })
         }
-
         event.preventDefault();
     }
 
@@ -145,7 +110,7 @@ const Login = () => {
                 </form>
             }
 
-            <button onClick={handleGoogleSignIn} className='m-4 px-5 btn btn-info'>
+            <button onClick={googleSignIn} className='m-4 px-5 btn btn-info'>
                 <img src={GoogleLogo} alt="" className='google-logo' />
             Continue with Google
             </button>
---
+++ src/components/Login/LoginManager.js
@@ -0,0 +1,63 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+export const initializeLoginFramework = () => {
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+}
+
+// sign in using google
+export const handleGoogleSignIn = () => { 
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth()
+        .signInWithPopup(googleProvider)
+        .then(res => {
+            const { displayName, email } = res.user;
+            const signedInUser = {
+                name: displayName,
+                email: email
+            };
+            return signedInUser;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+// sign in using email and password
+export const createUserWithEmailAndPassword = (name, email, password) => {
+    return firebase.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(res => {
+            const newUserInfo = res.user;
+            newUserInfo.error = '';
+            newUserInfo.success = true;
+            return newUserInfo;
+        })
+        .catch((error) => {
+            const newUserInfo = {};
+            newUserInfo.success = false;
+            newUserInfo.error = error.message + '!!!';
+            return newUserInfo;
+        })
+}
+
+export const signInWithEmailAndPassword = (email, password) => {
+    return firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(res => {
+            const userInfo = res.user;
+            userInfo.error = '';
+            userInfo.success = true;
+            return userInfo;
+        })
+        .catch(error => {
+            const userInfo = {};
+            userInfo.error = error.message;
+            userInfo.success = false;
+            return userInfo;
+        })
+}
+// export default LoginManager;
